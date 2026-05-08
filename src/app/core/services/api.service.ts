931b6f88259204b64bb683e888c57f1d6d9ffe8b
Nourhan <1213084@edu.eg>
2026-05-08 06:02:31 +0300

    fix: lint errors and update progress tracker

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 export interface ApiOptions {
-  params?: HttpParams | { [param: string]: string | number | boolean | ReadonlyArray<string | number | boolean> };
-  headers?: HttpHeaders | { [header: string]: string | string[] };
+  params?: HttpParams | Record<string, string | number | boolean | readonly (string | number | boolean)[]>;
+  headers?: HttpHeaders | Record<string, string | string[]>;
   skipCache?: boolean;
 }
 
@@ -14,8 +14,7 @@ export interface ApiOptions {
 })
 export class ApiService {
   private apiUrl = environment.apiUrl;
-
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
   get<T>(path: string, options?: ApiOptions): Observable<T> {
     return this.http.get<T>(`${this.apiUrl}${path}`, {
@@ -24,21 +23,21 @@ export class ApiService {
     });
   }
 
-  post<T>(path: string, body: any, options?: ApiOptions): Observable<T> {
+  post<T>(path: string, body: unknown, options?: ApiOptions): Observable<T> {
     return this.http.post<T>(`${this.apiUrl}${path}`, body, {
       params: options?.params,
       headers: options?.headers
     });
   }
 
-  put<T>(path: string, body: any, options?: ApiOptions): Observable<T> {
+  put<T>(path: string, body: unknown, options?: ApiOptions): Observable<T> {
     return this.http.put<T>(`${this.apiUrl}${path}`, body, {
       params: options?.params,
       headers: options?.headers
     });
   }
 
-  patch<T>(path: string, body: any, options?: ApiOptions): Observable<T> {
+  patch<T>(path: string, body: unknown, options?: ApiOptions): Observable<T> {
     return this.http.patch<T>(`${this.apiUrl}${path}`, body, {
       params: options?.params,
       headers: options?.headers
